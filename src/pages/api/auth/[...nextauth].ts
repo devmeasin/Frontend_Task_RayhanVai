d0547d94836bf1af36b76c716aa011bc6509d46f
Easin Webpro <easinislam299@gmail.com>
2022-12-11 23:57:51 +0600

    :construction: [nextauth] user login and expire accesstoken generate accesstoken using refreshtoken . on going

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
 import "next-auth";
-import { login } from "api";
+import { parseCookies, setCookie } from 'nookies';
+
+import { login, refreshToken } from "http/index";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { IRefreshTokenData } from "http/type";
 
 
 declare module "next-auth" {
@@ -23,6 +26,32 @@ declare module "next-auth" {
   }
 }
 
+
+const refreshAccessToken =  async function (refreshTokenX : string) {
+
+  try {
+    
+    const { data }:any = await refreshToken({refresh_token: refreshTokenX, mode: 'json'});
+    // const cookies = parseCookies();
+    const tokenDataJson =  JSON.stringify({
+      access_token:data?.access_token,
+      refresh_token: data?.refresh_token,
+      accessTokenExpires : new Date(Date.now() + Number(data.expires) * 1000).toISOString(),
+    });
+
+    setCookie(null, 'tokenData', tokenDataJson);
+
+    return {
+      accessToken: data?.access_token,
+      refreshToken: data?.refresh_token,
+      accessTokenExpires:  new Date(Date.now() + Number(data.expires) * 1000).toISOString(),
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export const authOptions = {
 
   secret: process.env.NEXTAUTH_SECRET,
@@ -41,9 +70,11 @@ export const authOptions = {
          * We can expect it contains two properties: `email` and `password`
          */
           try {
+
             const { email, password } = credentials;
             const { data } :any = await login({ email: email, password: password });
             return { jwt: data?.access_token, user: data };
+
           } catch (error) {
             console.warn(error);
           }
@@ -68,14 +99,42 @@ export const authOptions = {
     //   return session;
     // },
 
-        jwt: async ({ token, user } :any) => {
-          user && (token.user = user)
-          return token
-         },
-        session: async ({ session, token } :any) => {
-            session.user = token.user
-            return session
-        }
+        // jwt: async ({ token, user } :any) => {
+        //   user && (token.user = user)
+        //   return token
+        //  },
+        // session: async ({ session, token } :any) => {
+        //     session.user = token.user
+        //     return session
+        // }
+
+
+        async jwt({ token, user, account }) {
+          // Initial sign in
+          if (account && user) {
+            return {
+              accessToken: account.access_token,
+              accessTokenExpires: Date.now() + account.expires_at * 1000,
+              refreshToken: account.refresh_token,
+              user,
+            }
+          }
+    
+          // Return previous token if the access token has not expired yet
+          if (Date.now() < token.accessTokenExpires) {
+            return token
+          }
+    
+          // Access token has expired, try to update it
+          return refreshAccessToken(token);
+        },
+        async session({ session, token }) {
+          session.user = token.user
+          session.accessToken = token.accessToken
+          session.error = token.error
+    
+          return session
+        },
 
   },
 
